b916a7b3d4dead116a737a76997444b5f9fd496d
Alexander Madonado <alexandermaldonadosoto@gmail.com>
2022-09-04 19:33:13 -0600

    Termine la portada y el navbar responsive y todo listo con version desktop y mobile

---
+++ js/navbar.js
@@ -0,0 +1,20 @@
+///////codigo del navbar para mobile
+const navBar = document.getElementById('navbar');
+
+function navbar() {
+  gsap.to("#navbar", {
+    duration: 0.5,
+    left: 0,
+  });
+  document.querySelector("#div").style.backgroundColor = "rgb(32 32 32 / 67%)";
+   navBar.classList.add('navbar--show');
+}
+
+function navbarReverse() {
+  gsap.to("#navbar", {
+    duration: 0.5,
+    left: -800,
+  });
+  document.querySelector("#div").style.backgroundColor = "transparent";
+  navBar.classList.remove('navbar--show');
+}
